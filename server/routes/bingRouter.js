7c17a3dc5db052eb83ca157083c6c8c51d853cb4
kitocole <kaenyitocole@gmail.com>
2021-08-26 19:31:36 -0700

    removed secrets

--- server/routes/bingRouter.js
+++ server/routes/bingRouter.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const queryController = require('../controllers/queryController.js');
 const router = express.Router();
-const API_KEY = 'ca7b702bf6c646449330d37df9ae1dc9';
+
 
 router.post('/', queryController.bingQuery, queryController.addResults, (req, res) => {
   // console.log(res.locals.webpages);
@@ -15,30 +15,4 @@ router.post('/', queryController.bingQuery, queryController.addResults, (req, re
 })
 
 
-// router.post('/', async (req, res) => {
-//   const query = req.body.query;
-
-//   // console.log(encodeURIComponent(query));
-//   const data = await bingQuery(query);
-//   // console.log(data);
-//   return res.status(200).json(data.webpages);
-// })
-
-// const bingQuery = async (query) => {
-//   let url = 'https://api.bing.microsoft.com/v7.0/search?q=';
-//   url = url.concat(encodeURIComponent(query));
-//   url = new URL(url);
-//   // console.log(url);
-//   const data = await fetch(url, {
-//     method: 'GET',
-//     headers:{
-//       'Ocp-Apim-Subscription-Key': API_KEY
-//     }
-//   }).then(response => response.json())
-//   .then(data => {
-//     return data;
-//   })
-//   .catch(err => console.log(err));
-//   return data;
-// }
 module.exports= router;
